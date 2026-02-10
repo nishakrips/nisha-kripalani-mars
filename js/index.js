e8c9c1d385dc25636200e5b6ea4baa305c9cb0a2
Nisha <kripalaninisha@gmail.com>
2026-02-10 11:02:24 -0500

    Week-13 lesson-11 assignment

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const today = new Date()
+const thisYear = today.getFullYear()
+const footerElement = document.createElement("footer")
+const copyright = document.createElement("p")
+copyright.innerHTML = `© Nisha Kripalani ${thisYear}`
+footerElement.appendChild(copyright)
+document.body.appendChild(footerElement)
+
+const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"]
+const skillsSection = document.getElementById("Skills")
+const skillsList = skillsSection.querySelector("ul")
+
+for (let i = 0; i < skills.length; i++) {
+  const skill = skills[i]
+  const skillListItem = document.createElement("li")
+  skillListItem.textContent = skill
+  skillsList.appendChild(skillListItem)
+}
+
+/*Not sure why this is not getting displayed as 2 columns per row */
+const skillsWithGridSection = document.getElementById("SkillsWithGrid")
+const skillsDiv = skillsWithGridSection.querySelector("div")
+for (let i = 0; i < skills.length; i++) {
+  const skill = skills[i]
+  const skillDivItem = document.createElement("div")
+  skillDivItem.textContent = skill
+  skillsDiv.appendChild(skillDivItem)
+}
